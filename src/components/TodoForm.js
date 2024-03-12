b1819a647813fa4817fe9823b548a871fe5fa494
mingfulsnack <minhnk2004@gmail.com>
2024-03-12 21:12:44 +0700

    Add Firebase

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -9,6 +9,7 @@ function TodoForm({
     text, setText,
     day, setDay,
     time, setTime,
+    todoProject, setTodoProject,
     projects,
     showButtons = false,
     setShowModal = false
@@ -61,11 +62,20 @@ function TodoForm({
                     </div>
                     <div className="projects">
                         {
+                            projects.length > 0 ?
                             projects.map( project => 
-                                <div className="project" key={project.id}>
+                                <div
+                                    className={`project ${todoProject === project.name ? "active" : ""}`}
+                                    onClick={() => setTodoProject(project.name)}
+                                    key={project.id}
+                                >
                                     {project.name}
                                 </div>    
                             )
+                            :
+                            <div style={{color:'#ff0000'}}>
+                                Please add a project before proceeding
+                            </div>
                         }
                     </div>
                 </div>
---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeCFKmR_zo8cXiczmNzbOoByKjeHpAmyw",
+  authDomain: "todo-app-53a4e.firebaseapp.com",
+  projectId: "todo-app-53a4e",
+  storageBucket: "todo-app-53a4e.appspot.com",
+  messagingSenderId: "1067734142541",
+  appId: "1:1067734142541:web:e89dab339468602ed31604"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
