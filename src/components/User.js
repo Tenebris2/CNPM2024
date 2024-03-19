a9a48c8ba3d0b37fe36af3dc441c4e6641ce3f10
mingfulsnack <minhnk2004@gmail.com>
2024-03-19 07:50:26 +0700

    add bootstrap

--- src/components/User.js
+++ src/components/User.js
@@ -9,7 +9,7 @@ function User(){
                 <img src={logo} alt="logo" />
             </div>
             <div className='info'>
-                <p>Code Explained</p>
+                <p>Hanoi Ghoul</p>
                 <a href="#">Logout!</a>
             </div>
         </div>
