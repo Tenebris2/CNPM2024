00bac146fd315b32265e9327fa13d3b5fc8c29ff
ShinyAri <tanhngusi39@gmail.com>
2024-03-19 08:24:20 +0700

    modify code style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import Sidebar from './components/Sidebar'
-import Main from './components/Main'
-import User from './components/User'
-import AddNewTodo from './components/AddNewTodo'
-import Calendar from './components/Calendar'
-import Projects from './components/Projects'
-import Todos from './components/Todos'
-import EditTodo from './components/EditTodo'
-
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
+import User from "./components/User";
+import AddNewTodo from "./components/AddNewTodo";
+import Calendar from "./components/Calendar";
+import Projects from "./components/Projects";
+import Todos from "./components/Todos";
+import EditTodo from "./components/EditTodo";
 
 function App() {
   return (
