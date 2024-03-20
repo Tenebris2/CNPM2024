730d3e69f7972cb4da51b25f7525055268b9bbc0
mingfulsnack <minhnk2004@gmail.com>
2024-03-20 17:01:01 +0700

    change in tasks

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,7 +2,9 @@ import React, { useContext } from "react";
 import { CalendarDate, CaretUp } from "react-bootstrap-icons";
 import { calendarItems } from "../constants";
 import { TodoContext } from "../context";
-
+import {FaTasks} from "react-icons/fa"
+import { MdDoubleArrow } from "react-icons/md";
+import { MdDoneAll } from "react-icons/md";
 function Calendar() {
   // CONTEXT
   const { setSelectedProject } = useContext(TodoContext);
@@ -11,8 +13,7 @@ function Calendar() {
     <div className="Calendar">
       <div className="header">
         <div className="title">
-          <CalendarDate size="18" />
-          <p>Calendar</p>
+          <p>Tasks</p>
         </div>
         <div className="btns">
           <span>
@@ -21,16 +22,19 @@ function Calendar() {
         </div>
       </div>
       <div className="items">
-        {calendarItems.map((item) => (
-          <div
-            className="item"
-            key={item}
-            onClick={() => setSelectedProject(item)}
-          >
-            {item}
-          </div>
-        ))}
-      </div>
+  <div className="item" onClick={() => setSelectedProject(calendarItems[0])}>
+    <FaTasks />
+    {calendarItems[0]}
+  </div>
+  <div className="item" onClick={() => setSelectedProject(calendarItems[1])}>
+  <MdDoubleArrow />
+    {calendarItems[1]}
+  </div>
+  <div className="item" onClick={() => setSelectedProject(calendarItems[2])}>
+  <MdDoneAll />
+    {calendarItems[2]}
+  </div>
+</div>
     </div>
   );
 }
