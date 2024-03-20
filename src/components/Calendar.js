3a491bfbeb821e2cb3133759f31ba285846c4884
ShinyAri <tanhngusi39@gmail.com>
2024-03-20 18:10:51 +0700

    update sidebar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { CalendarDate, CaretUp } from "react-bootstrap-icons";
 import { calendarItems } from "../constants";
 import { TodoContext } from "../context";
-import {FaTasks} from "react-icons/fa"
+import { FaTasks } from "react-icons/fa";
 import { MdDoubleArrow } from "react-icons/md";
 import { MdDoneAll } from "react-icons/md";
 function Calendar() {
@@ -22,19 +22,28 @@ function Calendar() {
         </div>
       </div>
       <div className="items">
-  <div className="item" onClick={() => setSelectedProject(calendarItems[0])}>
-    <FaTasks />
-    {calendarItems[0]}
-  </div>
-  <div className="item" onClick={() => setSelectedProject(calendarItems[1])}>
-  <MdDoubleArrow />
-    {calendarItems[1]}
-  </div>
-  <div className="item" onClick={() => setSelectedProject(calendarItems[2])}>
-  <MdDoneAll />
-    {calendarItems[2]}
-  </div>
-</div>
+        <div
+          className="item"
+          onClick={() => setSelectedProject(calendarItems[0])}
+        >
+          <FaTasks size={14} />
+          <p>{calendarItems[0]}</p>
+        </div>
+        <div
+          className="item"
+          onClick={() => setSelectedProject(calendarItems[1])}
+        >
+          <MdDoubleArrow size={14} />
+          <p>{calendarItems[1]}</p>
+        </div>
+        <div
+          className="item"
+          onClick={() => setSelectedProject(calendarItems[2])}
+        >
+          <MdDoneAll size={14} />
+          <p>{calendarItems[2]}</p>
+        </div>
+      </div>
     </div>
   );
 }
