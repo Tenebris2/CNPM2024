fa587bb7d9236220d045189bc37268cefc2b758d
mingfulsnack <minhnk2004@gmail.com>
2024-03-25 21:51:13 +0700

    sign in, sign up, sign out

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,82 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate instead of useHistory
 import { auth } from "../firebase/FirebaseForLogin";
-import { signInWithEmailAndPassword, signInWithEmailLink } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [authUser, setAuthUser] = useState(null);
+    const navigate = useNavigate(); // Initialize useNavigate
+
     const signIn = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
-        .then( (userCredential) => {
-            console.log(userCredential);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+            .then((userCredential) => {
+                // Redirect to "/app" if login successful
+                navigate("/app");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
+    const navigateToSignUp = () => {
+        navigate("/signup");
+    };
+    useEffect(() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user);
+            } else {
+                setAuthUser(null);
+            }
+        });
+        return () => {
+            listen();
+        };
+    }, []);
+
+    const userSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("sign out successful");
+            })
+            .catch((error) => console.log(error));
+    };
+
     return (
         <div className="sign-in-container">
             <form onSubmit={signIn}>
                 <h1>Login in to your account</h1>
-                <input type="email" placeholder="Enter your email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)}> 
-                </input>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
 
-                <input type="password" placeholder="Enter your password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)}> 
-                </input>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
 
                 <button type="submit">Login</button>
+                <button onClick={navigateToSignUp}>Sign up</button>
             </form>
+
+            <div>
+                {authUser ? (
+                    <>
+                        <p>{`Signed in as ${authUser.email}`}</p>
+                        <button onClick={userSignOut}>Sign out</button>
+                    </>
+                ) : (
+                    <p>Signed Out</p>
+                )}
+            </div>
         </div>
     );
 };
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,32 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 import { auth } from "../firebase/FirebaseForLogin";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+
 const SignUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate(); // Initialize useNavigate
+
     const signUp = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
-        .then( (userCredential) => {
-            console.log(userCredential);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+            .then((userCredential) => {
+                console.log(userCredential);
+                // Redirect to "/" after successful sign up
+                navigate("/app");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
+
     return (
         <div className="sign-in-container">
             <form onSubmit={signUp}>
                 <h1>Create your account</h1>
-                <input type="email" placeholder="Enter your email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)}> 
-                </input>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
 
-                <input type="password" placeholder="Enter your password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)}> 
-                </input>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
 
                 <button type="submit">Sign up</button>
             </form>
