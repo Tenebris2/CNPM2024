c4033b67454596de40e5b65d47495dc171609b77
ShinyAri <tanhngusi39@gmail.com>
2024-04-03 08:45:44 +0700

    add css to sign up tab

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,49 +2,49 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 import { auth } from "../firebase/FirebaseForLogin";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import "./css/Signup.css"
+import "./css/Signup.css";
 const SignUp = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate(); // Initialize useNavigate
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); // Initialize useNavigate
 
-    const signUp = (e) => {
-        e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                console.log(userCredential);
-                // Redirect to "/" after successful sign up
-                navigate("/app");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        // Redirect to "/" after successful sign up
+        navigate("/app");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <div className="Signup">
-        <div className="auth-form-container">
-            <form className="login-form" onSubmit={signUp}>
-                <h1>Create your account</h1>
-                <input
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                ></input>
+  return (
+    <div className="Signup">
+      <div className="auth-form-container">
+        <form className="login-form" onSubmit={signUp}>
+          <h1>Sign up</h1>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
 
-                <input
-                    type="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                ></input>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
 
-                <button type="submit">Sign up</button>
-            </form>
-        </div>
-        </div>
-    );
+          <button type="submit">Sign up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
