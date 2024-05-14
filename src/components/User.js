b226c8bfd65e5e57812e13731ae10f0f03d823ed
mingfulsnack <minhnk2004@gmail.com>
2024-05-14 18:11:23 +0700

    Sign in as a user

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/ghould.png";
 import { auth } from "../firebase/FirebaseForLogin";
 import { onAuthStateChanged, signOut } from "firebase/auth";
@@ -6,14 +6,29 @@ import { useNavigate } from "react-router-dom";
 import { BoxArrowRight } from "react-bootstrap-icons";
 
 function User() {
+  const [authUser, setAuthUser] = useState(null);
   const navigate = useNavigate();
+
   const userSignOut = () => {
     signOut(auth)
-      .then((userCredential) => {
+      .then(() => {
         navigate("/");
       })
       .catch((error) => console.log(error));
   };
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => {
+      listen();
+    };
+  }, []);
 
   return (
     <div className="User">
@@ -21,11 +36,17 @@ function User() {
         <img src={logo} alt="logo" />
       </div>
       <div className="info">
-        <p>Hanoi Ghoul</p>
-        <button onClick={userSignOut}>
-          <BoxArrowRight size={20} />
-          <div className="textOut">Sign out</div>
-        </button>
+        {authUser ? (
+          <>
+            <p>{`Signed in as ${authUser.email}`}</p>
+            <button onClick={userSignOut}>
+              <BoxArrowRight size={20} />
+              <div className="textOut">Sign out</div>
+            </button>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
   );
